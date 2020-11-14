75d784f9111153fffd72c515184a2657d1523f35
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-11-13 20:00:09 -0800

    Minor fixes

--- process_frames.sh
+++ process_frames.sh
@@ -3,7 +3,7 @@
 . /opt/isce2/isce_env.sh
 . $slcp2cor_root/set_env_variable.sh
 
-$slcp2cor_root/script/look.py -i geom_masa ter/lat.rdr.full -o geom_master/lat.rdr.7r2a -r 7 -a 2
+$slcp2cor_root/script/look.py -i geom_master/lat.rdr.full -o geom_master/lat.rdr.7r2a -r 7 -a 2
 $slcp2cor_root/script/look.py -i geom_master/lon.rdr.full -o geom_master/lon.rdr.7r2a -r 7 -a 2
 
 $slcp2cor_root/script/slcstk2gamp.py -i SLC/ -o GAMP -lat geom_master/lat.rdr.7r2a -lon geom_master/lon.rdr.7r2a -r 7 -a 2 -bbox "34.6002832,34.6502392,-79.0801608,-78.9705888" -ssize 0.5
